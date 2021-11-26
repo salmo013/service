08128d54beb9c790e147bb36156ccabd594ce339
salmo0135 <lukesalmon13@gmail.com>
2021-11-25 20:26:48 -0800

    Update app.py

--- processing/app.py
+++ processing/app.py
@@ -49,14 +49,9 @@ def populate_stats():
         with open(EVENT_FILE) as jsonFile:
             jsonObject = json.load(jsonFile)
         last_updated = jsonObject[0]['Last_update']
-
         logger.info(f'{last_updated} taken from the start of processing')
-        # jsonFile = open(EVENT_FILE)
-        # jsonObject = json.load(jsonFile)
-        # last_updated = jsonObject[0]['Last_update']
-
-        logger.info(f'{last_updated} taken from the start of processing')
-
+        total1 = jsonObject[0]['Total of event 1']
+        total2 = jsonObject[0]['Total of event 2']
 
     else:
         spec_yaml= 'inmotion.yaml'
@@ -69,38 +64,18 @@ def populate_stats():
                 example_stats[items] = stats_dict[items]['example']
                 #print(items,' ',stats_dict[items]['example'])
             last_updated = example_stats['Last_update']
-
-    # read the stats
-
-
+            total1 = example_stats['Total of event 1']
+            total2 = example_stats['Total of event 2']
     # current time parameter
     now = datetime.datetime.now()
     time_string = now.strftime('%Y-%m-%dT%H:%M:%SZ')
-    ################
-    #time_string = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S") + "Z"
-    ###############
-    #print(last_updated)
-    #print(time_string)
-    # using request.get to get the data from database
-    
-    ##### Need to grab last update in other sections #########
-    #PARAMS = {'timestamp': last_updated}
+
     params = {'timestamp': last_updated, 'endtimestamp_datetime': time_string}
-    #############PARAMS UPDATED IN LAB (#######################)
-    #url1=app_config['eventstore']['url']
-    #url2=app_config['eventstore1']['url']
-    #logger.info(f'{params} ')
-    #response = requests.get(f'{url1}?timestamp={last_updated}&endtimestamp_datetime={time_string}')
-    #response2 = requests.get(f'{url2}?timestamp={last_updated}&endtimestamp_datetime={time_string}')
-    ####################################################
     # get requests from door motion
     response = requests.get(app_config['eventstore']['url'],params = params )
 
     #get requests from movement motion
     response2 = requests.get(app_config['eventstore1']['url'],params = params )
-    #response_motion = requests.get(app_config['eventstore1']['url'],params = PARAMS )
-    
-    #print(response2.json())
     # calculate the events recieved
     total1 =+ len(response.json())
     total2 =+ len(response2.json())
@@ -112,12 +87,11 @@ def populate_stats():
     else:
         dicts = response.json()
         dicts1 = response2.json()
-        #print(dicts)
         # how many times the couch was sit on
         value = list(filter(lambda dicts: dicts['location'] == 'Couch', dicts ))
         value1 = list(filter(lambda dicts1: dicts1['action'] == True , dicts1 ))
         # how many doors are left open
-        #value = list(filter(lambda dicts: dicts['item'] == 'Couch', dicts ))
+        
         
         jsonobj =[{'Total of event 1': total1, 'Total of event 2': total2 ,'Couch sits': len(value),'Door open': len(value1), 'Last_update': time_string }]
 
@@ -129,12 +103,7 @@ def populate_stats():
             with open(EVENT_FILE, "w") as outfile:
                 outfile.write(json_object)
                 logger.debug(json_object)
-            # outfile = open(EVENT_FILE, "w") 
-            # outfile.write(json_object)
-            # logger.debug(json_object)
-            # outfile.close()
-        total1= total1
-        total2= total2    
+               
 
         logger.info('end of processing')
         
@@ -157,21 +126,8 @@ def get_stats():
             jsonObject = json.load(jsonFile)
             response_dict = {'Total of event 1':jsonObject[0]['Total of event 1'] , 'Total of event 2': jsonObject[0]['Total of event 2'] ,'Couch sits': jsonObject[0]['Couch sits'],'Doors open': jsonObject[0]['Door open'] , 'Last_update': jsonObject[0]['Last_update']}
             logger.info('request has completed') 
-            #print('jsonObject')
             logger.info(f'{jsonObject}')
             return response_dict, 200 
-        # jsonFile =  open(EVENT_FILE, 'r')
-        # now = datetime.datetime.now()
-        # time_string = now.strftime('%Y-%m-%dT%H:%M:%SZ')
-        # timestamp_datetime = datetime.datetime.strptime(time_string,"%Y-%m-%dT%H:%M:%SZ")
-
-        # jsonObject = json.load(jsonFile)
-        # response_dict = {'Total of event 1':jsonObject[0]['Total of event 1'] , 'Total of event 2': jsonObject[0]['Total of event 2'] ,'Couch sits': jsonObject[0]['Couch sits'],'Doors open': jsonObject[0]['Door open'] , 'Last_update': jsonObject[0]['Last_update']}
-        # logger.info('request has completed') 
-        #     #print('jsonObject')
-        # logger.info(f'{jsonObject}')
-        # jsonFile.close()
-        # return response_dict, 200 
              
     else:
         error = "file does not exist"
